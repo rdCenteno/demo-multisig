e70eea4ab8138bfd4f113a3e4a4b1c67f488c88f
rdCenteno <ronnydemera@gmail.com>
2023-06-14 16:08:15 +0200

    deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,24 +1,25 @@
 async function main() {
-  // ethers is available in the global scope
-  [deployer, addr1] = await ethers.getSigners();
-  console.log(
-    "Deploying the contracts with the account:",
-    await deployer.getAddress()
-  );
+    const [deployer, addr1, addr2] = await ethers.getSigners();
+    const owners = [deployer.address, addr1.address, addr2.address];
+    const numConfirmations = 2;
+    console.log(
+        "Deploying the contracts with the account:",
+        await deployer.getAddress()
+    );
 
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+    console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const Contract = await ethers.getContractFactory("Contract");
-  const contract = await Contract.deploy(addr1.address);
-  await contract.deployed();
+    const MultiSigWallet = await ethers.getContractFactory("MultiSigWallet");
 
-  console.log("Wallet address:", contract.address);
-  console.log("Wallet owner:", addr1.address);
+    const multiSigWallet = await MultiSigWallet.deploy(owners, numConfirmations);
+    await multiSigWallet.deployed();
+
+    console.log("Contract address:", multiSigWallet.address);
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
